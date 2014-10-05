1334f676b421788a94e6292af7ac7d4385657bd1
Robert Scherer <ich@robertscherer.de>
2014-10-05 13:03:05 +0200

    adding custom Caskfile.sh

---
+++ Caskfile.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+brew tap caskroom/versions
+
+# Apps
+apps=(
+  dropbox
+  google-chrome
+  transmit
+  appcleaner
+  firefox
+  hazel
+  qlmarkdown
+  spotify
+  flash
+  iterm2
+  sublime-text3
+  virtualbox
+  qlstephen
+  tower
+  vlc
+  nvalt
+  quicklook-json
+  skype
+  transmission
+  sequel-pro
+  textmate
+  transmit
+  kaleidoscope
+  mysqlworkbench
+  colloquy
+  hipchat
+  istat-menus
+  crashplan
+  httpscoop
+  forklift
+  filebot
+  titanium-studio
+  vmware-fusion
+)
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
