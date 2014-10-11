b4c1181f9cb6bcb354b7f4cd40b7e697b7dd8174
Robert Scherer <ich@robertscherer.de>
2014-10-11 19:39:26 +0200

    remove obsolete Brewfile and Caskfile, limit files copied to home folder in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "Brewfile" --exclude "Caskfile" --exclude ".vimrc" --exclude ".vim" -avh --no-perms . ~;
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "Brewfile.sh" --exclude "Caskfile.sh" --exclude "SetupNotes.md" --exclude ".vimrc" --exclude ".vim" --exclude "init" --exclude "bin" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
